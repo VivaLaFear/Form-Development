79880a62947561339dbd3f4f5e7e45d7ce2e21b4
Jormungandrx <171705931+Jormungandrx@users.noreply.github.com>
2026-04-10 15:34:49 -0600

    Add Firebase configuration and initialization
    This file contains the Firebase configuration and initialization code.

---
+++ Form-Development/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMSLRFTa_onXrsAlfujM5RKhbE5XnNYdU",
+  authDomain: "cprg303-firebase-52c43.firebaseapp.com",
+  projectId: "cprg303-firebase-52c43",
+  storageBucket: "cprg303-firebase-52c43.firebasestorage.app",
+  messagingSenderId: "282509144493",
+  appId: "1:282509144493:web:8f9c201cdc2f70fc9c3124"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
